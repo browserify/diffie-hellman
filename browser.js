b0dcb0610ae831a6e711e6ab1bd9a7cb75378d22
Daniel Cousens <dcousens@users.noreply.github.com>
2015-01-28 00:01:59 +1100

    browser: consistent indentation

--- browser.js
+++ browser.js
@@ -7,7 +7,7 @@ function getDiffieHellman(mod) {
   var prime = new Buffer(primes[mod].prime, 'hex');
   var gen = new Buffer(primes[mod].gen, 'hex');
 
-	return new DH(prime, gen);
+  return new DH(prime, gen);
 }
 
 function createDiffieHellman(prime, enc, generator, genc) {
@@ -16,23 +16,23 @@ function createDiffieHellman(prime, enc, generator, genc) {
 		generator = enc;
 		enc = undefined;
 	}
-
+	
 	enc = enc || 'binary';
 	genc = genc || 'binary';
 	generator = generator || new Buffer([2]);
-
+	
 	if (!Buffer.isBuffer(generator)) {
 		generator = new Buffer(generator, genc);
 	}
-
+	
 	if (typeof prime === 'number') {
 		return new DH(generatePrime(prime, generator), generator, true);
 	}
-
+	
 	if (!Buffer.isBuffer(prime)) {
 		prime = new Buffer(prime, enc);
 	}
-
+	
 	return new DH(prime, generator, true);
 }
 
